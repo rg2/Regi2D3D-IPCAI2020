87c3c63cc1794448fcca7f2718a5996407e311fe
Robert Grupp <robert.grupp@gmail.com>
2020-09-23 18:36:26 -0400

    Updating label projection to produce annotations that could be used to train a CNN.   * output seg. in proj. data format to include lands   * addign preprocessing (crop/log trans./downsampling)   * write out processed intensity image   * rot to pat up

--- apps/proj_3d_labels_into_2d/xreg_proj_3d_labels_into_2d_main.cpp
+++ apps/proj_3d_labels_into_2d/xreg_proj_3d_labels_into_2d_main.cpp
@@ -31,6 +31,7 @@
 #include "xregH5ProjDataIO.h"
 #include "xregOpenCVUtils.h"
 #include "xregHDF5.h"
+#include "xregProjPreProc.h"
 
 #include "IPCAICommon.h"
 
@@ -59,14 +60,28 @@ int main(int argc, char* argv[])
   po.set_min_num_pos_args(7);
   po.set_help_epilogue(fmt::format("\nIPCAI version: {}", IPCAIVersionStr()));
 
-  po.add("cam-ds", 'd', ProgOpts::kSTORE_DOUBLE, "cam-ds",
-         "Downample the camera (2D image) dimensions by this factor, this is done before "
+  po.add("pat-up", ProgOpts::kNO_SHORT_FLAG, ProgOpts::kSTORE_TRUE, "pat-up",
+         "If necessary, rotate the output projections to have the patient oriented \"up.\" "
+         "This also removes the rot-pat-up field from the output projections.")
+    << false;
+
+  po.add("preproc", ProgOpts::kNO_SHORT_FLAG, ProgOpts::kSTORE_TRUE, "preproc",
+         "Apply standard preprocessing such as cropping and, if the \"intensity\" flag is set, "
+         "log intensity conversion.")
+    << false;
+
+  po.add("ds-factor", 'd', ProgOpts::kSTORE_DOUBLE, "ds-factor",
+         "Downsample the camera (2D image) dimensions by this factor, this is done before "
          "projection. e.g. 0.5 --> shrink ny half, 1.0 --> no downsampling.")
     << 1.0;
 
   po.add("proj-idx", 'p', ProgOpts::kSTORE_UINT32, "proj-idx",
          "Index of the projection to project to (the projection data file may store several projections)")
     << ProgOpts::uint32(0);
+
+  po.add("intensity", 'i', ProgOpts::kSTORE_STRING, "intensity",
+         "Save a copy of the intensity projection data at this path, after cropping, downsampling, etc.")
+    << "";
 
   po.add("no-ras2lps", ProgOpts::kNO_SHORT_FLAG, ProgOpts::kSTORE_TRUE, "no-ras2lps",
          "Do NOT convert RAS to LPS (or LPS to RAS) for the 3D landmarks; "
@@ -106,13 +121,21 @@ int main(int argc, char* argv[])
 
   const std::string multi_chan_2d_seg_path = (po.pos_args().size() > 7) ? po.pos_args()[7] : std::string();
 
-  const double cam_ds = po.get("cam-ds");
+  const double ds_factor = po.get("ds-factor");
   
-  const bool do_ds = std::abs(cam_ds - 1.0) > 0.001;
+  const bool do_ds = std::abs(ds_factor - 1.0) > 0.001;
 
   const bool ras2lps = !po.get("no-ras2lps").as_bool();
 
   const size_type proj_idx = po.get("proj-idx").as_uint32();
+
+  const std::string dst_intens_proj_path = po.get("intensity");
+  
+  const bool save_intens_proj = !dst_intens_proj_path.empty();
+
+  const bool apply_preproc = po.get("preproc");
+
+  const bool rot_pat_up = po.get("pat-up");
 
   vout << "reading label map..." << std::endl;
   auto ct_labels = ReadITKImageFromDisk<itk::Image<unsigned char,3>>(seg_3d_path);
@@ -133,13 +156,38 @@ int main(int argc, char* argv[])
   PrintLandmarkMap(lands_3d, vout);
 
   vout << "reading projection data..." << std::endl;
-  auto cam = ReadCamModelsFromProjDataFromDisk(proj_data_path)[proj_idx];
+  DeferredProjReader pd_reader(proj_data_path);
+  
+  xregASSERT(proj_idx < pd_reader.num_projs_on_disk());
+
+  auto pd = pd_reader.proj_data_F32()[proj_idx];
+
+  if (save_intens_proj)
+  {
+    pd.img = pd_reader.read_proj_F32(proj_idx);
+  }
+
+  if (apply_preproc)
+  {
+    vout << "projection preprocessing..." << std::endl;
+    
+    ProjPreProc preproc;
+    preproc.set_debug_output_stream(vout, verbose);
+    
+    preproc.input_projs = { pd };
+
+    preproc();
+
+    pd = preproc.output_projs[0];
+  }
 
   if (do_ds)
   {
-    vout << "downsampling camera model..." << std::endl;
-    cam = DownsampleCameraModel(cam, cam_ds);
+    vout << "downsampling input projection data..." << std::endl;
+    pd = DownsampleProjData(pd, ds_factor);
   }
+
+  const auto& cam = pd.cam;
 
   vout << "reading pelvis pose..." << std::endl;
   const FrameTransform pelvis_pose = ReadITKAffineTransformFromFile(pelvis_pose_path);
@@ -153,8 +201,56 @@ int main(int argc, char* argv[])
   auto proj_labels = ProjectHipLabels(ct_labels, cam, pelvis_pose, left_femur_pose, right_femur_pose,
                                       ProgOptsDepthRayCasterFactory(po), verbose, vout);
  
+  vout << "creating proj. data. struct for the single-channel segmentation..."  << std::endl;
+  ProjDataU8 seg_pd;
+  
+  {
+    const FrameTransform vol_to_cam = pelvis_pose.inverse();
+
+    seg_pd.cam = cam;
+    seg_pd.img = std::get<0>(proj_labels);
+
+    for (const auto& lkv : lands_3d)
+    {
+      seg_pd.landmarks.emplace(lkv.first, cam.phys_pt_to_ind_pt(vol_to_cam * lkv.second).head(2));
+    }
+  }
+
+  if (rot_pat_up && pd.rot_to_pat_up && (*pd.rot_to_pat_up != ProjDataRotToPatUp::kZERO))
+  {
+    xregASSERT(*pd.rot_to_pat_up == ProjDataRotToPatUp::kONE_EIGHTY);
+    
+    vout << "rotating projections for patient up..." << std::endl;
+
+    ModifyForPatUp(seg_pd.img.GetPointer(), *pd.rot_to_pat_up);
+
+    for (auto& lkv : seg_pd.landmarks)
+    {
+      lkv.second[0] = static_cast<CoordScalar>(cam.num_det_cols - 1) - lkv.second[0];
+      lkv.second[1] = static_cast<CoordScalar>(cam.num_det_rows - 1) - lkv.second[1];
+    }
+
+    if (save_intens_proj)
+    {
+      ModifyForPatUp(pd.img.GetPointer(), *pd.rot_to_pat_up);
+      pd.rot_to_pat_up.reset();
+    }
+  }
+  else
+  {
+    seg_pd.rot_to_pat_up = pd.rot_to_pat_up;
+  }
+
+  if (save_intens_proj)
+  {
+    pd.landmarks = seg_pd.landmarks;
+  
+    vout << "writing processed intensity proj. data to disk..." << std::endl;
+    WriteProjDataH5ToDisk(pd, dst_intens_proj_path);
+  }
+
   vout << "writing single-channel segmentation to disk..." << std::endl;
-  WriteITKImageToDisk(std::get<0>(proj_labels).GetPointer(), single_chan_2d_seg_path);
+  WriteProjDataH5ToDisk(seg_pd, single_chan_2d_seg_path);
 
   if (!multi_chan_2d_seg_path.empty())
   {

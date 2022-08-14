c6c3d35a68e7b052263c47b52030d99ffefeaf93
Robert Grupp <robert.grupp@gmail.com>
2022-08-14 00:04:45 -0700

    changing specification of CMAKE_INSTALL_PREFIX to cmake command line arg

--- .github/workflows/win_regi2d3d_ipcai2020_build.cmd
+++ .github/workflows/win_regi2d3d_ipcai2020_build.cmd
@@ -4,8 +4,6 @@ setlocal
 
 set CUR_DRIVE="%~d0" || EXIT /b
 
-set CMAKE_PREFIX_PATH:PATH="%CUR_DRIVE%\tmp\xreg_build;%CUR_DRIVE%\usr\local;%CMAKE_PREFIX_PATH%" || EXIT /b
-
 set "PATH=%CUR_DRIVE%\tmp\cmake-3.22.2-windows-x86_64\bin;%CUR_DRIVE%\tmp\ninja-bin;%PATH%" || EXIT /b
 
 cd regi2d3d-ipcai2020 || EXIT /b
@@ -15,6 +13,7 @@ mkdir build || EXIT /b
 cd build || EXIT /b
 
 cmake -G Ninja ^
+    -DCMAKE_PREFIX_PATH:PATH="%CUR_DRIVE%\tmp\xreg_build:%CUR_DRIVE%\usr\local" ^
     -DCMAKE_CXX_STANDARD:STRING="11" ^
     -DCMAKE_BUILD_TYPE:STRING="Release" ^
     -DBUILD_SHARED_LIBS:BOOL="OFF" ^

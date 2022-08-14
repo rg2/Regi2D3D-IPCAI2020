594595269fe6d7910aaf96060b43584d5bcc604c
Robert Grupp <robert.grupp@gmail.com>
2022-08-14 15:22:56 -0700

    changing search and generator vars

--- .github/workflows/win_regi2d3d_ipcai2020_build.cmd
+++ .github/workflows/win_regi2d3d_ipcai2020_build.cmd
@@ -4,7 +4,7 @@ setlocal
 
 set CUR_DRIVE="%~d0" || EXIT /b
 
-set "PATH=%CUR_DRIVE%\tmp\cmake-3.22.2-windows-x86_64\bin;%CUR_DRIVE%\tmp\ninja-bin;%PATH%" || EXIT /b
+set "PATH=%CUR_DRIVE%\tmp\cmake-3.22.2-windows-x86_64\bin;%PATH%" || EXIT /b
 
 cd regi2d3d-ipcai2020 || EXIT /b
 
@@ -12,8 +12,9 @@ mkdir build || EXIT /b
 
 cd build || EXIT /b
 
-cmake -G Ninja ^
-    -DCMAKE_PREFIX_PATH:PATH="%CUR_DRIVE%\tmp\xreg_build:%CUR_DRIVE%\usr\local" ^
+cmake -G Ninja -DCMAKE_MAKE_PROGRAM:PATH="%CUR_DRIVE%/tmp/ninja-bin/ninja.exe" ^
+    -Dxreg_DIR:PATH="%CUR_DRIVE%/tmp/xreg_build:" ^
+    -DCMAKE_PREFIX_PATH:PATH="%CUR_DRIVE%/usr/local" ^
     -DCMAKE_CXX_STANDARD:STRING="11" ^
     -DCMAKE_BUILD_TYPE:STRING="Release" ^
     -DBUILD_SHARED_LIBS:BOOL="OFF" ^

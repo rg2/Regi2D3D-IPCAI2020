62da174b5ea9a1143b4e0c0d6bb51e796428196e
Robert Grupp <robert.grupp@gmail.com>
2022-08-14 16:53:55 -0700

    changing the ninja path again

--- .github/workflows/win_regi2d3d_ipcai2020_build.cmd
+++ .github/workflows/win_regi2d3d_ipcai2020_build.cmd
@@ -4,7 +4,7 @@ setlocal
 
 set CUR_DRIVE="%~d0" || EXIT /b
 
-set "PATH=%CUR_DRIVE%\tmp\cmake-3.22.2-windows-x86_64\bin;%PATH%" || EXIT /b
+set "PATH=%CUR_DRIVE%\tmp\cmake-3.22.2-windows-x86_64\bin;%CUR_DRIVE%\tmp\ninja-bin;%PATH%" || EXIT /b
 
 cd regi2d3d-ipcai2020 || EXIT /b
 
@@ -12,7 +12,7 @@ mkdir build || EXIT /b
 
 cd build || EXIT /b
 
-cmake -G Ninja -DCMAKE_MAKE_PROGRAM:PATH="%CUR_DRIVE%/tmp/ninja-bin/ninja.exe" ^
+cmake -G Ninja ^
     -Dxreg_DIR:PATH="%CUR_DRIVE%/tmp/xreg_build:" ^
     -DCMAKE_PREFIX_PATH:PATH="%CUR_DRIVE%/usr/local" ^
     -DCMAKE_CXX_STANDARD:STRING="11" ^

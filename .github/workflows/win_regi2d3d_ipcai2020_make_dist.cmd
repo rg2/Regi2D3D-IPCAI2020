4c891480e324dea1ba501a7bc9571df2b9e5521f
Robert Grupp <robert.grupp@gmail.com>
2022-08-14 19:16:31 -0700

    adding some quotes, adding echo on

--- .github/workflows/win_regi2d3d_ipcai2020_make_dist.cmd
+++ .github/workflows/win_regi2d3d_ipcai2020_make_dist.cmd
@@ -1,3 +1,5 @@
+echo on
+
 REM Create a .zip of the compiled binaries
 
 REM Use the drive where this script currently resides
@@ -10,17 +12,17 @@ SET "INSTALL_ROOT=%CUR_DRIVE%\usr\local"
 ECHO INSTALL_ROOT = %INSTALL_ROOT%
 
 SET "XREG_NAME=regi2d3d-ipcai2020-win64"
-MKDIR %XREG_NAME% || EXIT /b
-MKDIR %XREG_NAME%\bin || EXIT /b
+MKDIR "%XREG_NAME%" || EXIT /b
+MKDIR "%XREG_NAME%\bin" || EXIT /b
 
-XCOPY /E %INSTALL_ROOT%\bin\OpenCL.dll %XREG_NAME%\bin\
-XCOPY /E %INSTALL_ROOT%\bin\tbb.dll %XREG_NAME%\bin\
-XCOPY /E %INSTALL_ROOT%\bin\setup-xreg-vars.bat %XREG_NAME%\bin\
-XCOPY /E %INSTALL_ROOT%\bin\ffmpeg.exe %XREG_NAME%\bin\
-XCOPY /E %INSTALL_ROOT%\bin\xreg-ipcai-* %XREG_NAME%\bin\
+XCOPY /E "%INSTALL_ROOT%\bin\OpenCL.dll" "%XREG_NAME%\bin\"
+XCOPY /E "%INSTALL_ROOT%\bin\tbb.dll" "%XREG_NAME%\bin\"
+XCOPY /E "%INSTALL_ROOT%\bin\setup-xreg-vars.bat" "%XREG_NAME%\bin\"
+XCOPY /E "%INSTALL_ROOT%\bin\ffmpeg.exe" "%XREG_NAME%\bin\"
+XCOPY /E "%INSTALL_ROOT%\bin\xreg-ipcai-*" "%XREG_NAME%\bin\"
 
-XCOPY /E  regi2d3d-ipcai2020\README.md %XREG_NAME%\bin\
-XCOPY /E  regi2d3d-ipcai2020\LICENSE %XREG_NAME%\bin\
+XCOPY /E  regi2d3d-ipcai2020\README.md "%XREG_NAME%\bin\"
+XCOPY /E  regi2d3d-ipcai2020\LICENSE "%XREG_NAME%\bin\"
 
 REM GitHub runners have 7zip installed, so we will use that to create the .zip
-7z a -tzip %XREG_NAME%.zip %XREG_NAME%
+7z a -tzip "%XREG_NAME%.zip" "%XREG_NAME%"
